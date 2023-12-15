ed050a94e72a6553c1645249fbc0dd7f93169332
龙龙 <08183080@cumt.edu.cn>
2023-12-15 13:37:57 +0800

    dfs

--- src/dfs/L797_所有可能的路径.py
+++ src/dfs/L797_所有可能的路径.py
@@ -1,24 +1,28 @@
 from typing import List
 
-def dfs(graph: List[List[int]], num: int, n: int, path: List[int]):
-    if (num == n - 1):  # 终止条件 
-        path.append(num)
-        print(path)
-        return
-    # path.append(num)
-    for i in graph[num]:
-        path.append(i)
-        dfs(graph, i, n, path)
-        path.remove(i)
-
-
 class Solution:
+    def __init__(self):
+        self.ans = []
+
+    def dfs(self, graph: List[List[int]], num: int, n: int, path: List[int]):
+        if (num == n - 1):  # 终止条件 
+            self.ans.append(path[:])
+            # print(path)
+            return
+        # path.append(num)
+        for i in graph[num]:
+            path.append(i)
+            self.dfs(graph, i, n, path)
+            path.pop()
 
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         n = len(graph)
         path = list()
-        dfs(graph, 0, n, path)
+        path.append(0)
+        self.dfs(graph, 0, n, path)
+        print(self.ans)
+        return self.ans
 
 s = Solution()
-graph = [[1,2],[3],[3],[]]
+graph = [[4,3,1],[3,2,4],[3],[4],[]]
 s.allPathsSourceTarget(graph)
