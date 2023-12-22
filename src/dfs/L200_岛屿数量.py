02f28b3228b1ed4aa4b6245ae209aa834428274d
dragon <51738561+08183080@users.noreply.github.com>
2023-12-22 16:02:21 +0800

    dfs

--- src/dfs/L200_岛屿数量.py
+++ src/dfs/L200_岛屿数量.py
@@ -2,22 +2,41 @@ from typing import List
 
 def dfs(grid: List[List[str]], r: int, c: int):
     rows = len(grid)
-    cols = len(grid[0])        
-    if not (0 <= r < rows and 0 <= c < cols):
+    cols = len(grid[0])
+    if not(0 <= r < rows and 0 <= c < cols):
         return
-    if grid[r][c] == 0:
+    if grid[r][c] == "0":
         return
-    if grid[r][c] != 1:
-        return
+    grid[r][c] = "0"
+    dfs(grid, r - 1, c)
+    dfs(grid, r + 1, c)
     dfs(grid, r, c - 1)
     dfs(grid, r, c + 1)
-    dfs(grid, r - 1, c)
-    dfs(grid, r, c + 1)
+
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         """
         海洋上, 有几座岛屿呢...
+        填海造陆, 神州陆沉...
+        总有一些比记忆更重要的东西...
         """
         rows = len(grid)
         cols = len(grid[0])
+        island_num = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    island_num += 1
+                    dfs(grid, r, c)
+        return island_num
+
+s = Solution()
+grid = grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+print(s.numIslands(grid))
