fd73495b3e01cc7555826a8b5c5982f3e5cf730a
08183080 <houlong.xie@intel.com>
2024-02-11 20:56:14 +0800

    [func time]add func time measure

--- src/sorting/1bubbleSort.py
+++ src/sorting/1bubbleSort.py
@@ -1,14 +1,29 @@
+from time import time
 from typing import List
 
 nums = [1, 2, 56, 3, 11, 34, 99]
 
+def time_func(func, *args, **kwargs):
+    """
+    Time the execution of a function with params
+    """
+    start = time() 
+    output = func(*args, **kwargs)
+    end = time()
+    if int(end - start) > 0:
+        print(f'{func.__name__} runtime: {(end - start):0.4f}s')
+    else:
+        print(f'{func.__name__} runtime: {(end - start):0.4f}s')
+    return output
+
 def bubble_sort(nums: List[int]):
-    n = len(nums) 
-    for i in  range(1, n):
+    n = len(nums)
+    for i in range(1, n):
         for j in range(0, n - i):
-            if nums[j] > nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
 
 if __name__ == "__main__":
-    bubble_sort(nums=nums)
-    print(nums)
+    output = time_func(bubble_sort, nums)
+    print(output)
---
+++ src/sorting/2selectionSort.py
@@ -0,0 +1,4 @@
+from typing import List
+
+nums = [1, 33, 2, 6, 19, 100]
+
