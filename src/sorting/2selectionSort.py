5e9550031d27007c628a4fe12f1c98eb18fe8885
08183080 <houlong.xie@intel.com>
2024-02-11 21:07:25 +0800

    [sort] add selection sort

--- src/sorting/2selectionSort.py
+++ src/sorting/2selectionSort.py
@@ -2,3 +2,17 @@ from typing import List
 
 nums = [1, 33, 2, 6, 19, 100]
 
+def select_sort(nums: List[int]):
+    n = len(nums)
+    for i in range(0, n-1):
+        for j in range(i, n):
+            min = i
+            if nums[j] < nums[i]:
+                min = j
+            if min != i:
+                nums[i], nums[min] = nums[min], nums[i]
+    return nums
+
+if __name__ == "__main__":
+    ans = select_sort(nums=nums)
+    print(ans)
