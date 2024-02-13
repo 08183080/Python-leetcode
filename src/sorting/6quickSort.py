58ff715670173933f1357b26fbd7328d121d8c1c
08183080 <houlong.xie@intel.com>
2024-02-13 10:52:23 +0800

    [sort] quick sort begin

---
+++ src/sorting/6quickSort.py
@@ -0,0 +1,11 @@
+import random
+from typing import List
+
+nums = [random.randint(0, 100) for i in range(10)]
+print('[*] original nums array:',nums)
+
+def quick_sort(nums: List[int]) -> List[int]:
+    pass
+
+if __name__ == "__main__":
+    quick_sort(nums=nums)
