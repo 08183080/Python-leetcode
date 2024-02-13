f3b4bf9ecf2d8b9d9f987230b99c42b58be55888
08183080 <houlong.xie@intel.com>
2024-02-13 10:39:18 +0800

    [sort] merge sort add random nums

--- src/sorting/5mergeSort.py
+++ src/sorting/5mergeSort.py
@@ -1,27 +1,29 @@
+import random
 from typing import List
 
-nums = [1, 33, 2, 24, 8, 99, 100, 1, 2, 1]
+nums = [random.randint(0, 100) for i in range(10)]
+print('[*] original nums array:',nums)
 
-def merge_sort(nums: List[int]):
+def merge_sort(nums: List[int]) -> List[int]:
     if len(nums) == 1:
         return nums
-    middle = len(nums) // 2
-    left, right = nums[0: middle], nums[middle:]
+    mid = len(nums) // 2
+    left, right = nums[0: mid], nums[mid:]
     return merge(merge_sort(left), merge_sort(right))
 
-def merge(left: List[int], right: List[int]):
-    result = []
+def merge(left: List[int], right: List[int]) -> List[int]:
+    res = []
     while left and right:
-        if left[0] <= right[0]:
-            result.append(left.pop(0))
+        if left[0] < right[0]:
+            res.append(left.pop(0))
         else:
-            result.append(right.pop(0))
+            res.append(right.pop(0))
     while left:
-        result.append(left.pop(0))
+        res.append(left.pop(0))
     while right:
-        result.append(right.pop(0))
-    return result
+        res.append(right.pop(0))
+    return res
 
 if __name__ == "__main__":
     res = merge_sort(nums=nums)
-    print(res)
+    print('[*] after merge sort: ', res)
