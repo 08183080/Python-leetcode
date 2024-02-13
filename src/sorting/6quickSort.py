454021986d6bc7d69dd6cbfd3d04f800e7ccace0
08183080 <houlong.xie@intel.com>
2024-02-13 15:41:10 +0800

    [sort] quick sort

--- src/sorting/6quickSort.py
+++ src/sorting/6quickSort.py
@@ -5,7 +5,16 @@ nums = [random.randint(0, 100) for i in range(10)]
 print('[*] original nums array:',nums)
 
 def quick_sort(nums: List[int]) -> List[int]:
-    pass
+    """
+    code inspired by [alogitithm graph books]
+    """
+    if len(nums) < 2:
+        return nums
+    pivot = nums[0]
+    left = [i for i in nums[1:] if i < pivot]
+    right = [i for i in nums[1:] if i > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right) 
 
 if __name__ == "__main__":
-    quick_sort(nums=nums)
+    res = quick_sort(nums=nums)
+    print('[*] after quick sort: ',res)
