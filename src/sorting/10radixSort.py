21fb04a44bbaeb1167da08c619660087078ffa71
08183080 <houlong.xie@intel.com>
2024-02-14 13:10:15 +0800

    [sort] begin to write radix sort simply

---
+++ src/sorting/10radixSort.py
@@ -0,0 +1,9 @@
+import random
+from typing import List
+
+"""
+begin: https://www.programiz.com/dsa/radix-sort
+"""
+
+nums = [random.randint(0, 100) for i in range(10)] # an array, len = 10, value = [0, 9]
+print('[*] original nums array:', nums)
