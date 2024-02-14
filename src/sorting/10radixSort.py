af9ddbd923ea810dbba9ea4d5a61373a5747b56d
08183080 <houlong.xie@intel.com>
2024-02-14 18:56:23 +0800

    [sort] radix has some problems

--- src/sorting/10radixSort.py
+++ src/sorting/10radixSort.py
@@ -7,3 +7,7 @@ begin: https://www.programiz.com/dsa/radix-sort
 
 nums = [random.randint(0, 100) for i in range(10)] # an array, len = 10, value = [0, 9]
 print('[*] original nums array:', nums)
+
+def radix_sort(nums: List[int]):
+    n = len(nums)
+    
